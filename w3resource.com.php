84445b162679658d03ee39233c1dc93e8d6461e3
Asya <liilmrbrr@gmail.com>
2016-02-06 22:54:40 +0300

    first commit

--- w3resource.com.php
+++ w3resource.com.php
@@ -25,4 +25,7 @@
 	echo ("<h3>$var</h3>");
 	echo ("PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, 
 		HTML embedded server-side scripting language and is especially suited for web development.");
+	
+	
+	// commit to github
 ?>
